76fefedddf92e28f3e469297e65326f80ead23b7
gait206 <s1081369@student.windesheim.nl>
2014-11-21 13:13:10 +0100

    added index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" type="text/css" href="stylesheet.css">
+    </head>
+    <body>
+        <form action="verwerk.php" method="POST">
+            Afdeling:<input type="text" name="afdeling">
+            <input type="submit" value="Zoek">
+        </form>
+    </body>
+</html>
