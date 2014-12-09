b7e9b0644c6a18a6d577c83041f4d31a19cc2bcc
erjanfct <erjankroese@MacBook-Pro-van-Erjan-6.local>
2014-12-09 23:23:58 +0100

    footer gemaakt
    footer gemaakt

--- index.php
+++ index.php
@@ -42,7 +42,28 @@ if (!existCookie($cookiename)) {
             </div>
 
             <div class="footer">
-
+            <table class="footertable" border=0 width="100%">
+            <tr class="footerheader" height="30" >
+            <td>Vos en Vos Tissue</td>
+            <td>Contact</td> 
+            <td>Bedrijfgegevens</td>
+            <td>Follow me</td>
+            </tr><tr>
+            
+            <td>Conradstraat 2E</td>
+            <td class="tablelink">Email: <a href="mailto:info@vos-vos.nl">info@vos-vos.nl</a></td>
+            <td>BTW nr.: NL xxxx.xx.xxx.xxx</td>
+     		<td class="tablelink"><a href="https://www.facebook.com/vosenvos.schoonmaakbedrijf" target="_blank">Volg ons op facebook</td>
+            </tr><tr>
+            
+            <td>8004 DA  Zwolle</td>
+            <td>Telefoonnummer: 038-4529329</td> 
+            <td>KvK nr.: xxxxxxxx</td>
+            </tr><tr>
+            
+            </tr>
+            
+            </table>
             </div>
         </div>
     </body>
