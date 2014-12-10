38f9bb108ea32de17c6d14123eb68b551283eaec
erjanfct <erjankroese@MacBook-Pro-van-Erjan-6.local>
2014-12-10 18:46:57 +0100

    .
    .

--- registratie/Registratievoltooid.php
+++ registratie/Registratievoltooid.php
@@ -15,7 +15,7 @@ if (!existCookie($cookiename)) {
         <link rel="stylesheet" type="text/css" href="../css/registratievoltooid.css">
         <link href='http://fonts.googleapis.com/css?family=Oxygen' rel='stylesheet' type='text/css'>
         <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
-        <meta http-equiv="refresh" content="5;url=http://localhost/index.php" />
+        <meta http-equiv="refresh" content="5;url=http://localhost:8080/index.php" />
 
     </head>
     <body>
