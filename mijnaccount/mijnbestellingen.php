5f2f3210cdbfb444175a830e7936ab1a62d46580
Danny Franken <S1078476@student.windesheim.nl>
2014-12-10 10:45:49 +0100

    mijn bestelling heeft nu query
    mijn bestelling heeft nu een query voor het ophalen van een bestelling
    met een bepaalde status.

--- mijnaccount/mijnbestellingen.php
+++ mijnaccount/mijnbestellingen.php
@@ -1,17 +1,58 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+<?php
+session_start();
+include('../functies.php');
+$link = connectDB();
+?>
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
+        <link rel="stylesheet" type="text/css" href="../css/main.css">
+        <link href='http://fonts.googleapis.com/css?family=Oxygen' rel='stylesheet' type='text/css'>
+        <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <div class="container">
+
+            <div class="header">
+
+                <div class="logo">
+                    <img class="logo" src="../plaatjes/logo.png">
+                </div>
+                <div class="login">
+                    <?php
+                    include('../login/loginscherm.php');
+                    ?>
+                </div>
+            </div>
+
+            <?php
+            define('THIS_PAGE', 'mijnbestellingen');
+            include('../menu.php');
+            ?>
+
+            <div class="content" id="main_content">
+                <!--kijken of de login klopt-->
+                <?php
+//                if (!validToken($link)) {
+//                    header('Location: ../index.php');
+//                }
+//                $email = getEmail($link);
+                $email = "dick@gmail.com";
+                var_dump($email);
+                $result = mysqli_query($link, "SELECT * FROM Bestelling AS B JOIN Klant AS K ON K.klantnr = K.klantnr WHERE email = '$email' AND status ='kots'");
+                $bestelling = mysqli_fetch_assoc($result);
+                var_dump($bestelling);
+                
+                ?>
+            </div>
+
+            <div class="footer">
+
+            </div>
+        </div>
     </body>
 </html>
+<?php
+mysqli_close($link);
+?>
