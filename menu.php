ad23a7f01f0e6e6d2416cd7d17c76d0231caa78d
gait206 <s1081369@student.windesheim.nl>
2014-12-10 12:32:50 +0100

    Account dropdown toegevoegd

--- menu.php
+++ menu.php
@@ -8,6 +8,7 @@ $menuitem4 = '<a href="/reinigingsmiddelen.php">Reinigingsmiddelen</a>';
 $menuitem5 = '<a href="/schoonmaakmateriaal.php">Schoonmaakmateriaal</a>';
 $menuitem6 = '<a href="/winkelwagen.php" align="right">( '.countItems(getCookie("winkelmandje")).' ) <img height="20" src="/plaatjes/winkelmandje-menu.png"></a>';
 $menuitem7 = '<a href="/administratie/productbeheer.php">Admin</a>';
+$menuitem8 = '<a href="/mijnaccount/mijnaccount.php">Mijn account</a>';
  
 switch (THIS_PAGE) {
  
@@ -57,10 +58,17 @@ print( '<ul class="dropdown">
 <li>'.$menuitem3.'</li>
 <li>'.$menuitem4.'</li>
 <li>'.$menuitem5.'</li>');
+if(validToken($link)){
 if(userLevel(getEmail($link), $link) == "Admin") {
 print('<li>'.$menuitem7.'<ul>'
         . '<li><a href="/administratie/productbeheer.php">Productbeheer</a></li>'
         . '<li><a href="/administratie/bestellingbeheer.php">Bestellingbeheer</a></li></ul></li>');
+} else {
+    print('<li>'.$menuitem8.'<ul>'
+        . '<li><a href="/mijnaccount/mijngegevens.php">Mijn gegevens</a></li>'
+        . '<li><a href="/mijnaccount/mijnbestellingen.php">Mijn bestellingen</a></li>'
+        . '<li><a href="/mijnaccount/bestelgeschiedenis.php">Bestelgeschiedenis</a></li></ul></li>');
+}
 }
 print('<li>'.$menuitem6.'</li>');
 print('</ul>');
