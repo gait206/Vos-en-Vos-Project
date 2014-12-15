dc3ce85a4cf20e8f02a0c87811123ec54f51ec43
erjanfct <erjankroese@90-112.dhcp.windesheim.nl>
2014-12-15 19:21:53 +0100

    it works!
    it works!

---
+++ test1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Naamloos document</title>
+</head>
+
+<body>
+<table;">
+<tr  onclick="document.location = 'http://www.test1.nl/';"><td>test1</td><td>test2</td></tr>
+<tr onclick="document.location = 'http://www.test2.nl/';><td>test3</td><td>test4</td></tr>
+</table>
+</body>
+</html>
