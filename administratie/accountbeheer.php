f521bea36be547aacdac7f3ac946000328453ed3
ThePizzaguy <oscar.van.zanten@windesheim.nl>
2014-12-15 16:31:32 +0100

    accountbeheer basis af
    alleen pagina count en zoekfunctie

--- administratie/accountbeheer.php
+++ administratie/accountbeheer.php
@@ -44,8 +44,10 @@
 						if (!empty($_POST["actie"])) {
 							$actie = $_POST["actie"];
 							if ($actie == "Verwijderen") {
-								$email = $_POST["email"];
-                                           mysqli_query($link, 'DELETE FROM klant WHERE email = "' . $email . '";DELETE FROM gebruiker WHERE email = "' . $email . '";'); 
+								$klantnr = $_POST["klantnr"];
+                                           mysqli_query($link, 'DELETE FROM klant WHERE klantnr = '.$klantnr.'; '); 
+										   print(mysqli_error($link));	
+										   mysqli_query($link, 'DELETE FROM gebruiker WHERE klantnr = '.$klantnr.';');
 										   print(mysqli_error($link));	
 							}
 
@@ -108,8 +110,37 @@
 							. '</form></table>'
 							.  '<input form="toevoegen" type="submit" name="actie" class="button" value="' .$waarde. '">'
 						);
-						} else {
-						
+						} 
+						if($actie == "bekijken"){
+							$klantnr = $_POST["klantnr"];
+							$result = mysqli_query($link, 'SELECT * FROM klant k, gebruiker g WHERE k.klantnr = g.klantnr and g.klantnr ="' . $_POST["klantnr"] . '";');
+							$row = mysqli_fetch_assoc($result);
+							$email = $row["email"];
+							$voornaam =  $row["voornaam"];
+							$achternaam = $row["achternaam"];
+							$telnummer = $row["telnummer"];	
+							$mobnummer = $row["mobnummer"];
+							$bedrijf = $row["bedrijfsnaam"];
+							$kvknummer = $row["kvknummer"];
+							$btwnummer = $row["btwnummer"];
+							$adres = $row['adres'];
+							$plaats = $row['plaats'];
+							$postcode = $row['postcode'];
+							$level = $row['level'];
+							
+							print('<table>'
+							. '<tr><td>Email:</td><td>'. $email.'</td></tr>'
+							. '<tr><td>Voornaam:</td><td>'. $voornaam.'</td></tr>'
+							. '<tr><td>achternaam:</td><td>'. $achternaam.'</td></tr>'
+							. '<tr><td>Telefoon nummer:</td><td>'.$telnummer.'</td></tr>'
+							. '<tr><td>Mobiel nummer:</td><td>'.$mobnummer.'</td></tr>'
+							. '<tr><td>Bedrijf:</td><td>'.$bedrijf.'</td></tr>'	
+							. '<tr><td>KVKnummer:</td><td>'.$kvknummer.'</td></tr>'	
+							. '<tr><td>BTWnummer:</td><td>'.$btwnummer.'</td></tr>'
+							. '<tr><td>Adres:</td><td>'. $adres ." " . $plaats ." ". $postcode.'</td></tr>'
+							. '<tr><td>Level:</td><td>'.$level.'</td></tr>'	
+							. '</table>'
+							);
 						}
 											
 						$result = mysqli_query($link, 'SELECT * FROM gebruiker g, klant k where g.klantnr =k.klantnr');
@@ -125,7 +156,7 @@
 									. '<td>' . $row["level"] . '</td>'
 									. '<td><form action="" method="POST" class="table_administratie_button" ><input type="hidden" name="klantnr" value="' . $row["klantnr"] . '"><input type="submit" name="actie" value="Verwijderen" onClick="return checkDelete()"></form></td>'
 									. '<td><form action="" method="POST" class="table_administratie_button" ><input type="hidden" name="klantnr" value="' . $row["klantnr"] . '"><input type="submit" name="actie" value="Aanpassen">	</form></td>'
-									. '<td><form action="" method="POST" class="table_administratie_button" ><input type="hidden" name="userid" value="' . $row["klantnr"] . '"><input type="submit" name="actie" value="bekijken"></form></td></tr>');
+									. '<td><form action="" method="POST" class="table_administratie_button" ><input type="hidden" name="klantnr" value="' . $row["klantnr"] . '"><input type="submit" name="actie" value="bekijken"></form></td></tr>');
 							$row = mysqli_fetch_assoc($result);
 						}
 						print("</table>");
