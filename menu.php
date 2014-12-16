b79d16ff3c393982e1c86c3d84c5d18158c74c8e
gait206 <s1081369@student.windesheim.nl>
2014-12-16 11:39:36 +0100

    besteldatum normaal weergegeven

--- menu.php
+++ menu.php
@@ -76,10 +76,11 @@ if(userLevel(getKlantnr($link), $link) == "Admin") {
 print('<li>'.$menuitem7.'<ul>'
         . '<li style="width:auto; border-right:none;"><a href="/administratie/productbeheer.php">Productbeheer</a></li>'
         . '<li style="width:auto; border-right:none;"><a href="/administratie/bestellingbeheer.php">Bestellingbeheer</a></li>'
-		. '<li style="width:auto; border-right:none;"><a href="/administratie/accountbeheer.php">Accountbeheer</a></li></ul></li>');
+	. '<li style="width:auto; border-right:none;"><a href="/administratie/accountbeheer.php">Accountbeheer</a></li>'
+        . '<li style="width:auto; border-right:none;"><a href="/mijnaccount/mijnaccount.php">Mijn gegevens</a></li></ul></li>');
 } else {
     print('<li>'.$menuitem8.'<ul>'
-        . '<li style="width:auto; border-right:none;"><a href="/mijnaccount/mijngegevens.php">Mijn gegevens</a></li>'
+        . '<li style="width:auto; border-right:none;"><a href="/mijnaccount/mijnaccount.php">Mijn gegevens</a></li>'
         . '<li style="width:auto; border-right:none;"><a href="/mijnaccount/mijnbestellingen.php">Mijn bestellingen</a></li>'
         . '<li style="width:auto; border-right:none;"><a href="/mijnaccount/bestelgeschiedenis.php">Bestelgeschiedenis</a></li></ul></li>');
 }
