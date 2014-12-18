810d94fb932eb0a38e3f2cc011199aa68c9970ff
erjanfct <erjankroese@89-201.dhcp.windesheim.nl>
2014-12-18 16:00:17 +0100

    footer aangepast
    footer aangepast

--- footer.php
+++ footer.php
@@ -3,18 +3,19 @@
             <td>Vos en Vos Tissue</td>
             <td>Contact</td> 
             <td>Bedrijfgegevens</td>
-            <td>Follow me</td>
+            <td>Social media</td>
             </tr><tr>
             
             <td>Conradstraat 2E</td>
             <td class="tablelink">Email: <a href="mailto:info@vos-vos.nl">info@vos-vos.nl</a></td>
             <td>BTW nr.: NL xxxx.xx.xxx.xxx</td>
-     		<td class="tablelink"><a href="https://www.facebook.com/vosenvos.schoonmaakbedrijf" target="_blank">Volg ons op facebook</td>
+     		<td class="tablelink"><a href="https://www.facebook.com/vosenvos.schoonmaakbedrijf" target="_blank">Like ons op Facebook</td>
             </tr><tr>
             
             <td>8004 DA  Zwolle</td>
             <td>Telefoonnummer: 038-4529329</td> 
             <td>KvK nr.: xxxxxxxx</td>
+            <td class="tablelink"><a href="https://www.twitter.com/vosenvos.schoonmaakbedrijf" target="_blank">Volg ons op Twitter</td>
             </tr><tr>
             
             </tr>
